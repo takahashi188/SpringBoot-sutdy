1d5dbf4c2710cdca7760b05a1a6b52d2c5bd7b04
takahashi <takahashi@exec.co.jp>
2026-09-02 10:43:52 +0900

    ユーザー一覧画面の状態をブラウザで管理する方法の実装

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -11,5 +11,21 @@ import Header from "./components/Header.vue";
     </main>
   </div>
 </template>
+<!-- <template>
+  <div class="min-h-screen bg-gray-100 flex flex-col">
+    <Header />
+
+    <main class="flex-1 max-w-6xl mx-auto w-full p-6 overflow-hidden">
+  
+  <RouterView v-slot="{ Component, route }">
+    <KeepAlive>
+      <component :is="Component" v-if="route.meta.Keepalive" />
+    </KeepAlive>
+
+    <component :is="Component" v-if="!route.meta.Keepalive" />
+  </RouterView>
+    </main>
+  </div>
+</template> -->
 
 <style scoped></style>
